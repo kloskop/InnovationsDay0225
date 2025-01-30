0ee46428975114b3a940e08fe6289f82c34b8b96
Pablo <kloskopawel@gmail.com>
2025-01-30 23:41:34 +0100

    port env setting

--- PushMeBackend/index.js
+++ PushMeBackend/index.js
@@ -4,7 +4,7 @@ const { google } = require("googleapis");
 const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
